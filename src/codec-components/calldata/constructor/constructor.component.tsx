3c8010dc94e20b63e650015109f87ee78ba045b8
Cliff Zhang <cliffzhang08@gmail.com>
2022-11-22 12:22:55 -0500

    Display more values

--- src/codec-components/calldata/constructor/constructor.component.tsx
+++ src/codec-components/calldata/constructor/constructor.component.tsx
@@ -6,6 +6,12 @@ export interface ConstructorProps {
 
 export function Constructor({ data }: ConstructorProps): JSX.Element {
   const contractName = data.class.typeName;
+  const args = data.arguments.length > 0 ? "(...)" : "()";
 
-  return <code>new {contractName}</code>;
+  return (
+    <code>
+      new {contractName}
+      {args}
+    </code>
+  );
 }
--- src/codec-components/calldata/function/function.component.tsx
+++ src/codec-components/calldata/function/function.component.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import type { FunctionDecoding, Format } from "@truffle/codec";
 
 export interface FunctionProps {
@@ -11,15 +12,60 @@ export function Function({ data }: FunctionProps): JSX.Element {
     const { type, name } = data.abi.inputs[index];
     let value;
     if (arg.value.kind === "value") {
-      if (arg.value.type.typeClass === "uint") {
-        value = (arg.value as Format.Values.UintValue).value.asBN.toString();
+      switch (arg.value.type.typeClass) {
+        case "uint":
+          value = (arg.value as Format.Values.UintValue).value.asBN.toString();
+          break;
+        case "int":
+          value = (arg.value as Format.Values.IntValue).value.asBN.toString();
+          break;
+        case "bool":
+          value = (
+            arg.value as Format.Values.BoolValue
+          ).value.asBoolean.toString();
+          break;
+        case "bytes":
+          value = (arg.value as Format.Values.BytesValue).value.asHex;
+          break;
+        case "address":
+          value = (arg.value as Format.Values.AddressValue).value.asAddress;
+          break;
+        case "fixed":
+          value = (arg.value as Format.Values.FixedValue).value.asBig;
+          break;
+        case "ufixed":
+          value = (arg.value as Format.Values.UfixedValue).value.asBig;
+          break;
+        case "string":
+          const isValid =
+            (arg.value as Format.Values.StringValue).value.kind === "valid";
+          if (isValid) {
+            value = (arg.value.value as Format.Values.StringValueInfoValid)
+              .asString;
+          } else {
+            value = (arg.value.value as Format.Values.StringValueInfoMalformed)
+              .asHex;
+          }
+          break;
+        case "array":
+          value = "[...]";
+          break;
+        case "function":
+          value = "externalFunctionValue";
+          break;
+        case "tuple":
+          value = "(...)";
+          break;
+        default:
+          value = "?";
       }
     }
 
     return (
-      <span key={index} data-tooltip={`${type} ${name}`}>
-        {value}
-      </span>
+      <Fragment key={index}>
+        <span data-tooltip={`${type} ${name}`}>{value}</span>
+        {index < data.arguments.length - 1 ? ", " : ""}
+      </Fragment>
     );
   });
 
