24ef4d14b8d8849eaecc295a453e1a0910068611
Cliff Zhang <cliffzhang08@gmail.com>
2023-03-23 17:14:05 +0000

    Use js instead of css to handle `CodeTooltip` hover behavior

--- src/codec-components/common/code-tooltip.tsx
+++ src/codec-components/common/code-tooltip.tsx
@@ -1,4 +1,4 @@
-import { useId } from "react";
+import { useId, useRef, useEffect } from "react";
 import styles from "./code-tooltip.module.scss";
 
 interface CodeTooltipProps {
@@ -6,16 +6,67 @@ interface CodeTooltipProps {
   dataOnHover: string;
 }
 
+const hideDelayMs = 150;
+
 export function CodeTooltip({
   data,
   dataOnHover
 }: CodeTooltipProps): JSX.Element {
   const id = `tooltip-${useId()}`;
+  const dataElementRef = useRef<HTMLElement>(null);
+  const dataOnHoverElementRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (!dataElementRef.current || !dataOnHoverElementRef.current) return;
+    const dataElement = dataElementRef.current;
+    const dataOnHoverElement = dataOnHoverElementRef.current;
+
+    let entered = false;
+    const handleDataElementMouseEnter = () => {
+      entered = true;
+      dataOnHoverElement.classList.add(styles["show"]);
+    };
+    const handleDataOnHoverElementMouseEnter = () => {
+      entered = true;
+    };
+    const handleMouseLeave = () => {
+      entered = false;
+      setTimeout(() => {
+        if (!entered) dataOnHoverElement.classList.remove(styles["show"]);
+      }, hideDelayMs);
+    };
+    dataElement.addEventListener("mouseenter", handleDataElementMouseEnter);
+    dataOnHoverElement.addEventListener(
+      "mouseenter",
+      handleDataOnHoverElementMouseEnter
+    );
+    dataElement.addEventListener("mouseleave", handleMouseLeave);
+    dataOnHoverElement.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      dataElement.removeEventListener(
+        "mouseenter",
+        handleDataElementMouseEnter
+      );
+      dataOnHoverElement.removeEventListener(
+        "mouseenter",
+        handleDataOnHoverElementMouseEnter
+      );
+      dataElement.removeEventListener("mouseleave", handleMouseLeave);
+      dataOnHoverElement.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
 
   return (
     <span className={styles["wrapper"]}>
-      <code aria-describedby={id}>{data}</code>
-      <code id={id} className={styles["data-on-hover"]}>
+      <code ref={dataElementRef} aria-describedby={id}>
+        {data}
+      </code>
+      <code
+        ref={dataOnHoverElementRef}
+        id={id}
+        className={styles["data-on-hover"]}
+      >
         {dataOnHover}
       </code>
     </span>
