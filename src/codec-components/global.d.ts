b7a99bd708a4cd060b10ed205b5887fd52bd97e6
Cliff Zhang <cliffzhang08@gmail.com>
2023-04-07 15:53:02 +0200

    Add Codec type to globalThis

--- src/codec-components/global.d.ts
+++ src/codec-components/global.d.ts
@@ -1,5 +1,7 @@
+import type * as _Codec from "@truffle/codec";
 import type { Format as _Format } from "@truffle/codec";
 
 declare global {
+  export { _Codec as Codec };
   export { _Format as Format };
 }
