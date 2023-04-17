808930cea130cc271bd73e2bff6a322ee99e2bdb
cliffoo <cliffoo@users.noreply.github.com>
2023-04-17 15:50:43 +0200

    Remove unused assignments

--- src/codec-components/utils/type-guards/value-and-result/type-value-contract.ts
+++ src/codec-components/utils/type-guards/value-and-result/type-value-contract.ts
@@ -1,6 +1,6 @@
 import { valueAndResultTypeGuardHelper } from "@/utils/type-guards/value-and-result/helper";
 
-const [isTypeValueContract, _, __, typeValueContractGuards] =
+const [isTypeValueContract, , , typeValueContractGuards] =
   valueAndResultTypeGuardHelper<Format.Values.TypeValueContract, any, any>(
     data =>
       data.type.typeClass === "type" && data.type.type.typeClass === "contract"
