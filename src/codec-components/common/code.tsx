a098284d32d8f07f121bf5415ff4f9505542038f
cliffoo <cliffoo@users.noreply.github.com>
2023-04-17 11:16:49 +0200

    Code component

---
+++ src/codec-components/common/code.tsx
@@ -0,0 +1,21 @@
+const colors = {
+  red: "#f85149",
+  blue: "#2f81f7",
+  purple: "#a371f7"
+};
+
+interface CodeProps {
+  children: string | string[];
+  color?: keyof typeof colors;
+  title?: string;
+}
+
+export function Code({ children, color, title }: CodeProps) {
+  return (
+    <span>
+      <code style={color && { color: colors[color] }} title={title}>
+        {children}
+      </code>
+    </span>
+  );
+}
