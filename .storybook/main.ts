7792bf9d29090abea3b4664d89e8f4e81c9e75d8
cliffoo <cliffoo@users.noreply.github.com>
2023-05-17 16:19:03 -0400

    Clean up storybook and update deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 export default {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
   framework: { name: "@storybook/react-vite", options: {} },
-  core: { disableTelemetry: true },
-  docs: { autodocs: true, defaultName: "docs" }
+  docs: { autodocs: true, defaultName: "docs" },
+  core: { disableTelemetry: true }
 } satisfies StorybookConfig;
--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,11 +1,10 @@
 import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming";
-const logoPath = require("./logo.svg") as string;
 
 addons.setConfig({
   theme: create({
     base: "dark",
-    brandImage: logoPath,
+    brandImage: require("./logo.svg"),
     brandTitle: "@truffle/codec-components"
   })
 });
