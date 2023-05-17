e1ef1ecd2b0f61f07dce8e81fdaa30a4d7d421a1
cliffoo <cliffoo@users.noreply.github.com>
2023-05-17 18:39:38 -0400

    Container fold/unfold The spirit is there but there are problems.

--- src/react/components/common/container.tsx
+++ src/react/components/common/container.tsx
@@ -1,24 +1,32 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { useState, useRef, useEffect, useLayoutEffect } from "react";
+import { useState, useRef, useEffect, useLayoutEffect, Children } from "react";
 import styles from "./container.module.scss";
 
 export interface ContainerProps {
   children: React.ReactNode;
   prefix?: React.ReactNode;
   suffix?: React.ReactNode;
+  foldable?: boolean;
 }
 export function Container({
   children,
   prefix,
-  suffix
+  suffix,
+  foldable
 }: ContainerProps): JSX.Element {
   const contentRef = useRef<HTMLDivElement>(null);
   const [wrapThreshold, setWrapThreshold] = useState<number>();
   const [wrap, setWrap] = useState<boolean>();
+  const [fold, setFold] = useState<boolean>();
+  foldable &&= Children.count(children) > 0;
 
   useEffect(
-    () => setWrapThreshold(contentRef.current!.scrollWidth),
-    [setWrapThreshold]
+    () =>
+      void setTimeout(
+        () => setWrapThreshold(contentRef.current!.scrollWidth),
+        0
+      ),
+    [setWrapThreshold, fold]
   );
 
   useLayoutEffect(() => {
@@ -30,6 +38,8 @@ export function Container({
     return () => observer.disconnect();
   });
 
+  const toggleFold = () => setFold(!fold);
+
   return (
     <div
       className={
@@ -37,8 +47,23 @@ export function Container({
       }
     >
       <div ref={contentRef} className={styles[`content${wrap ? "-wrap" : ""}`]}>
-        <div className={styles["prefix"]}>{prefix}</div>
-        <div className={styles["children"]}>{children}</div>
+        <div className={styles["prefix"]}>
+          {foldable && (
+            <button onClick={toggleFold} className={styles["toggle"]}>
+              {fold ? <>&#9656;</> : <>&#9662;</>}
+            </button>
+          )}
+          {prefix}
+        </div>
+        <div className={`${styles["children"]} ${fold ? styles["hide"] : ""}`}>
+          {children}
+        </div>
+        <button
+          onClick={toggleFold}
+          className={`${styles["toggle"]} ${fold ? "" : styles["hide"]}`}
+        >
+          &#8943;
+        </button>
         <div className={styles["suffix"]}>{suffix}</div>
       </div>
     </div>
