3b35b64559a18aa00895cc4558fe89681feb81b4
cliffoo <cliffoo@users.noreply.github.com>
2023-05-30 16:52:29 -0400

    Empty docs dir on build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "src/docs",
-  build: { outDir: "../../docs" },
+  build: { outDir: "../../docs", emptyOutDir: true },
   plugins: [react({ jsxRuntime: "classic" })]
 });
