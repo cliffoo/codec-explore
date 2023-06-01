590b514d6f333a6ae3327aeacbe877dd2adc7df5
cliffoo <cliffoo@users.noreply.github.com>
2023-06-01 13:06:13 -0400

    Export only react subpath For now utils subpath is for internal use only

--- scripts/build.js
+++ scripts/build.js
@@ -2,14 +2,11 @@ import { build } from "vite";
 import { Extractor, ExtractorConfig } from "@microsoft/api-extractor";
 
 (async () => {
-  for (const subpath of ["react", "utils"]) {
-    const config = ExtractorConfig.loadFileAndPrepare(
-      `scripts/api-extractor.${subpath}.json`
-    );
-    const result = Extractor.invoke(config, { localBuild: true });
-    if (!result.succeeded)
-      throw new Error("Failed to rollup declaration files.");
-  }
+  const config = ExtractorConfig.loadFileAndPrepare(
+    "scripts/api-extractor.json"
+  );
+  const result = Extractor.invoke(config, { localBuild: true });
+  if (!result.succeeded) throw new Error("Failed to rollup declaration files.");
 
   await build({
     configFile: false,
@@ -22,19 +19,6 @@ import { Extractor, ExtractorConfig } from "@microsoft/api-extractor";
       rollupOptions: {
         external: ["react", "react-dom"],
         output: { assetFileNames: "react/index[extname]" }
-      },
-      sourcemap: true,
-      emptyOutDir: false
-    }
-  });
-
-  await build({
-    configFile: false,
-    build: {
-      lib: {
-        entry: "src/utils/index.ts",
-        fileName: "utils/index",
-        formats: ["es"]
       },
       sourcemap: true,
       emptyOutDir: false
